f7cb07c2f2cf8085c74b67cb70c285d79f048ee2
sarahb6 <forsugar88@gmail.com>
2022-01-29 16:31:50 -0800

    done

---
+++ ShoppingList(array)/ShoppingList(array)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingList_array_
+{
+    class Program
+    {
+        public static void Add(LinkedList<string> input, string item)
+        {
+            input.AddLast(item);
+        }
+        public static void Remove(LinkedList<string> input, string item)
+        {
+            input.Remove(item);
+        }
+
+        public static string View(LinkedList<string> input)
+        {
+            string toReturn = "";
+            LinkedListNode<string> currentNode = input.First;
+
+            for (int i = 0; i<input.Count; i++)
+            {
+                toReturn += $"{currentNode.Value}\n";
+                currentNode = currentNode.Next;
+               
+            }
+            return toReturn;
+        }
+
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            Add(list, "apple");
+            Add(list, "popcorn");
+            Add(list, "chocolate");
+            Add(list, "flour");
+            Console.WriteLine(View(list));
+            Remove(list, "popcorn");
+            Console.WriteLine();
+            Console.WriteLine(View(list));
+        }
+    }
+}
