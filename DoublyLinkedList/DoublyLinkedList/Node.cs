5e6dd9825c4eb6e05ce7307788b7a70bc9bbb30d
sarahb6 <forsugar88@gmail.com>
2022-02-10 17:00:59 -0800

    1st draft

---
+++ DoublyLinkedList/DoublyLinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    class Node<T>
+    {
+        public Node<T> next;
+        public Node<T> previous;
+        public T input;
+
+        public Node(T input)
+        {
+            this.input = input;
+            next = null;
+            previous = null;
+        }
+    }
+}
---
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList<string> list = new DoubleLinkedList<string>();
+            Console.WriteLine(list.IsEmpty());
+            list.AddFirst("2");
+            list.AddFirst("1");
+            list.AddLast("3");
+            list.AddLast("4");
+            list.AddFirst("0");
+            Console.WriteLine(list);
+            list.AddAfter("7", list.head.next);
+            list.AddBefore("y", list.head.next.next.next);
+            Console.WriteLine(list);
+            Console.WriteLine(list.Count());
+            Console.WriteLine(list.IsEmpty());
+        }
+    }
+}
