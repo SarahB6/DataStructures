f2e719a0dfb8d271fcc27f74a3f1214d79a2dbe0
sarahb6 <forsugar88@gmail.com>
2022-02-24 16:12:41 -0800

    Done

--- QueueBackedByArray/QueueBackedByArray/Program.cs
+++ QueueBackedByArray/QueueBackedByArray/Program.cs
@@ -6,7 +6,18 @@ namespace QueueBackedByArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("hello");
+            Console.WriteLine(queue.Peek());
+            queue.Enqueue("ello");
+            Console.WriteLine(queue.Peek());
+            queue.Enqueue("llo");
+            Console.WriteLine(queue.Peek());
+            queue.Dequeue();
+            Console.WriteLine(queue.Peek());
+            queue.Dequeue();
+            Console.WriteLine(queue.Peek());
+
         }
     }
 }
---
+++ QueueBackedByArray/QueueBackedByArray/Queue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueBackedByArray
+{
+    class Queue<T>
+    {
+        T[] array = new T[1];
+        int index = 0;
+        int head = 0;
+        
+        public Queue()
+        {
+
+        }
+
+        public void Resize(int newSize)
+        {
+            T[] newA = new T[newSize];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newA[i] = array[i];
+            }
+            array = newA;
+        }
+
+        public void Enqueue(T input)
+        {
+            if(index==array.Length)
+            {
+                Resize(array.Length * 2);
+            }
+            array[index] = input;
+            index++;
+        }
+
+        public void Dequeue()
+        {
+            head++;
+        }
+
+        public T Peek()
+        {
+            return array[head];
+        }
+    }
+}
