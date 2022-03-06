3ff9a6525a9875db176003f21c1b4c8991db4dea
sarahb6 <forsugar88@gmail.com>
2022-03-06 13:45:29 -0800

    Done

---
+++ RecursionPractice/RecursionPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RecursionPractice
+{
+    class Program
+    {
+        public static int sumOfElements(int[] array, int index)
+        {
+            if(index == array.Length-1)
+            {
+                return array[array.Length-1];
+            }
+            return array[index] + sumOfElements(array, index+1);
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[4] { 1, 1, 1, 40 };
+            Console.WriteLine(sumOfElements(array, 0));
+        }
+    }
+}
