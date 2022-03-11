f60504ae4da0f0bb6c7497843b5c6c778257bc6f
sarahb6 <forsugar88@gmail.com>
2022-03-10 16:17:23 -0800

    Done

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MergeSort
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorter<int> sort = new Sorter<int>();
+            List<int> first = new List<int>();
+            first.Add(38);
+            first.Add(27);
+            first.Add(43);
+            first.Add(3);
+            first.Add(9);
+            first.Add(82);
+            first.Add(10);
+
+           List<int> result = sort.Sort(first);
+        }
+    }
+}
---
+++ MergeSort/MergeSort/Sorter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MergeSort
+{
+    class Sorter<T>
+        where T : IComparable<T>
+    {
+        
+        public Sorter()
+        {
+        }
+        public List<T> Sort(List<T> list)
+        {
+            int midpt = list.Count / 2;
+
+            List<T> left = new List<T>();
+            List<T> right = new List<T>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i < midpt)
+                {
+                    left.Add(list[i]);
+                }
+                else
+                {
+                    right.Add(list[i]);
+                }
+            }
+
+            if (list.Count < 2)
+            {
+                return list;
+            }
+            return Merge(Sort(left), Sort(right));
+        }
+
+        public List<T> Merge(List<T> first, List<T> second)
+        {
+            List<T> sorted = new List<T>();
+            int count1 = 0;
+            int count2 = 0;
+            for (int i = 0; i < second.Count + first.Count; i++)
+            {
+                if (count1 >= first.Count)
+                {
+                    sorted.Add(second[count2]);
+                    count2++;
+                }
+                else if (count2 >= second.Count)
+                {
+                    sorted.Add(first[count1]);
+                    count1++;
+                }
+                else if (first[count1].CompareTo(second[count2]) >= 0)
+                {
+                    sorted.Add(second[count2]);
+                    count2++;
+                }
+                else
+                {
+                    sorted.Add(first[count1]);
+                    count1++;
+                }
+            }
+            return sorted;
+
+        }
+
+    }
+}
