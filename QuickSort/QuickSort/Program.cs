102e578e5689b23e5d6867d56f28774b6eabb08e
sarahb6 <forsugar88@gmail.com>
2022-03-12 16:50:27 -0800

    Done

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace QuickSort
 {
     class Program
     {
+        public static bool testQuickSort(int iterations, int arraySize, int min, int max)
+        {
+            for (int a = 0; a < iterations; a++)
+            {
+                Random rand = new Random();
+                List<int> check = new List<int>();
+                for (int i = 0; i < arraySize; i++)
+                {
+                    check.Add(rand.Next(min, max));
+
+                }
+                QuickSorter<int> sort = new QuickSorter<int>();
+                sort.QuickSort(check, 0, check.Count-1);
+                int previous = check[0];
+                for (int i = 1; i < check.Count; i++)
+                {
+                    if (check[i - 1] > check[i])
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QuickSorter<int> sort = new QuickSorter<int>();
+            List<int> first = new List<int>();
+            first.Add(7);
+            first.Add(4);
+            first.Add(3);
+            first.Add(6);
+            first.Add(2);
+            first.Add(1);
+            first.Add(5);
+            sort.QuickSort(first, 0, first.Count-1);
+            bool response = testQuickSort(100, 9, 0, 100);
         }
     }
 }
---
+++ QuickSort/QuickSort/QuickSorter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSort
+{
+    class QuickSorter<T>
+        where T : IComparable<T>
+    {
+        public void QuickSort(List<T> input, int left, int right)
+        {
+            if (right - left <= 0)
+            {
+                return;
+            }
+
+            int pivot = Partition(input, left, right);
+            
+            QuickSort(input, left, pivot - 1);
+            QuickSort(input, pivot + 1, right);
+            
+        }
+
+        public int Partition(List<T> input, int left, int right)
+        {
+            T pivotValue = input[right];
+            int wall = left-1;
+            for(int i = 0; i<right-left; i++)
+            {
+                
+                if(input[left+i].CompareTo(pivotValue) < 0)
+                {
+                    wall++;
+                    T val = input[left + i];
+                    input[left + i] = input[wall];
+                    input[wall] = val;
+                    
+                }
+            }
+            input[right] = input[wall+1];
+            input[wall+1] = pivotValue;
+            return wall+1;
+        }
+
+    }
+}
