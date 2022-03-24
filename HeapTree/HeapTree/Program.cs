c03ddfb0aa9c252031bddf9c7df66a0b44860fc5
sarahb6 <forsugar88@gmail.com>
2022-03-24 16:22:36 -0700

    Done

---
+++ HeapTree/HeapTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            maxheap<int> heap = new maxheap<int>();
+            heap.Insert(1);
+            heap.Insert(2);
+            heap.Insert(3);
+            heap.Insert(10);
+            heap.Insert(13);
+            heap.Insert(9);
+            heap.Insert(9);
+            heap.Insert(7);
+            heap.Insert(17);
+
+            Console.WriteLine(heap.Print());
+
+        }
+    }
+}
---
+++ HeapTree/HeapTree/minheap.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapTree
+{
+    class minheap<T>
+    where T : IComparable<T>
+    {
+        List<T> list = new List<T>();
+        int remove = 0;
+        public void Insert(T toAdd)
+        {
+            list.Add(toAdd);
+            HeapifyUp(toAdd);
+        }
+
+        public int getLeftChild(int i)
+        {
+            return i * 2 + 1;
+        }
+
+        public int getRightChild(int i)
+        {
+            return i * 2 + 2;
+        }
+        public int getParent(int i)
+        {
+            if(i%2 == 1)
+            {
+                return i / 2;
+            }
+            return (i - 1) / 2;
+        }
+        public void HeapifyUp(T toAdd)
+        {
+            int add = list.Count - 1;
+            while(list[add].CompareTo(list[getParent(add)]) < 0) 
+            {
+                T val = list[add];
+                list[add] = list[getParent(add)];
+                list[getParent(add)] = val;
+                add = getParent(add);
+            }
+        }
+
+        public T Pop()
+        {
+            T root = list[0];
+            list[0] = list[list.Count - 1];
+            list.Remove(list[list.Count - 1]);
+            remove = 0;
+            while (getLeftChild(remove) < list.Count)
+            {
+                HeapifyDown();
+                remove++;
+            }
+            return root;
+        }
+
+        public void HeapifyDown()
+        {
+            if (list[remove].CompareTo(list[getLeftChild(remove)]) >= 0)
+            {
+                T val = list[remove];
+                list[remove] = list[getLeftChild(remove)];
+                list[getLeftChild(remove)] = val;
+                remove = getLeftChild(remove);
+            }
+            else if (getRightChild(remove) < list.Count)
+            {
+             if (list[remove].CompareTo(list[getRightChild(remove)]) >= 0)
+                {
+                    T val = list[remove];
+                    list[remove] = list[getRightChild(remove)];
+                    list[getRightChild(remove)] = val;
+                    remove = getRightChild(remove);
+                }
+            }
+        }
+
+        public string Print()
+        {
+            string s = "";
+            int count = list.Count;
+            for(int i = 0; i<count; i++)
+            {
+                s += $" {Pop()}";
+                //s += $" {list[i]}";
+            }
+            return s;
+        }
+    }
+}
