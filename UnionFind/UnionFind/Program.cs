61f916e9aef99b4b5b1d4d5031770806a7e9d0eb
sarahb6 <forsugar88@gmail.com>
2022-12-02 17:08:41 -0800

    QuickFind

---
+++ UnionFind/UnionFind/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using QuickFind;
+namespace UnionFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "a";
+            string s2 = "b";
+            string s3 = "c";
+            string s4 = "d";
+            string s5 = "e";
+            string s6 = "f";
+            string s7 = "g";
+            string s8 = "h";
+            string[] arr = new string[8] { s1, s2, s3, s4, s5, s6, s7, s8 };
+            QuickFind<string> qf = new QuickFind<string>(arr);
+            qf.Union(arr[2], arr[7]);
+            qf.Union(arr[0], arr[2]);
+            qf.Union(arr[0], arr[1]);
+            qf.Union(arr[1], arr[7]);
+            qf.Union(arr[4], arr[5]);
+            qf.Union(arr[5], arr[6]);
+            qf.Union(arr[3], arr[5]);
+
+            qf.Union(arr[1], arr[5]);
+            
+            
+
+
+        }
+    }
+}
---
+++ UnionFind/UnionFind/QuickFind.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickFind
+{
+    public class QuickFind<T>
+    {
+
+        private int[] sets;
+        private Dictionary<T, int> map;
+
+        public QuickFind(IEnumerable<T> items)
+        {
+            
+            map = new Dictionary<T, int>(items.Count());
+            sets = new int[items.Count()];
+            int count = 0;
+            foreach(T item in items)
+            {
+                map.Add(item, count);
+                count++;
+            }
+            for(int i = 0; i<items.Count(); i++)
+            {
+                sets[i] = i;
+            }
+        }
+
+        public int Find(T p) => sets[map[p]];
+
+        public bool Union(T p, T q)
+        {
+            if(AreConnected(p,q))
+            {
+                return false;
+            }
+            int val1 = sets[map[p]];
+            int val2 = sets[map[q]];
+            if(val1>val2)
+            {
+                for(int i = 0; i<sets.Length; i++)
+                {
+                    if(sets[i] == val1)
+                    {
+                        sets[i] = val2;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < sets.Length; i++)
+                {
+                    if (sets[i] == val2)
+                    {
+                        sets[i] = val1;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool AreConnected(T p, T q)
+        {
+            if(sets[map[p]] == sets[map[q]])
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+    }
+}
