bcabf614f3981d4254773892e5d175569a48ca34
sarahb6 <forsugar88@gmail.com>
2023-01-06 17:08:16 -0800

    Done

---
+++ BloomFilter/BloomFilter/BloomFilterer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BloomFilter
+{
+    public class BloomFilterer<T>
+    {
+        bool[] arr;
+        int arrSize;
+        List<Func<T, int>> funcList;
+        public BloomFilterer(int cap, Func<T, int>[] hashFuncs)
+        {
+            arr = new bool[cap];
+            arrSize = cap;
+            funcList = new List<Func<T, int>>();
+            for (int i = 0; i < hashFuncs.Length; i++)
+            {
+                funcList.Add(hashFuncs[i]);
+            }
+            funcList.Add(HashFuncOne);
+            funcList.Add(HashFuncTwo);
+            funcList.Add(HashFuncThree);
+        }
+
+        public void Insert(T item)
+        {
+            foreach (Func<T, int> curr in funcList)
+            {
+                arr[curr(item)] = true;
+            }
+        }
+        public bool ProbablyContains(T item)
+        {
+        foreach (Func<T, int> curr in funcList)
+        {
+            if (!arr[curr(item)])
+            {
+                return false;
+            }
+        }
+        return true;
+        }
+
+        private int HashFuncOne(T item)
+        {
+            int val = item.GetHashCode();
+            return val%arrSize;
+        }
+
+        private int HashFuncTwo(T item)
+        {
+            int val = item.GetHashCode();
+        return (val * 2)%arrSize;
+        }
+
+        private int HashFuncThree(T item)
+        {
+            int val = item.GetHashCode();
+        return (val * 3)%arrSize;
+        }
+    }
+}
---
+++ BloomFilter/BloomFilter/Tester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloomFilter
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            Func<char, int>[] list = new Func<char, int>[0];
+            BloomFilterer<char> bloom = new BloomFilterer<char>(100, list);
+
+            bloom.Insert('a');
+            bloom.Insert('b');
+            bloom.Insert('c');
+            bloom.Insert('d');
+            bloom.Insert('e');
+            bloom.Insert('f');
+            bloom.Insert('g');
+            bloom.Insert('h');
+            bloom.Insert('i');
+            bloom.Insert('j');
+            bloom.Insert('k');
+            bloom.Insert('l');
+            bloom.Insert('m');
+            bloom.Insert('n');
+            bloom.Insert('o');
+            bloom.Insert('p');
+            bloom.Insert('q');
+            bloom.Insert('r');
+            bloom.Insert('s');
+            bloom.Insert('t');
+            bloom.Insert('u');
+            bloom.Insert('v');
+            bloom.Insert('w');
+            bloom.Insert('x');
+            bloom.Insert('y');
+            bloom.Insert('z');
+            Console.WriteLine(bloom.ProbablyContains('a'));
+            Console.WriteLine(bloom.ProbablyContains('-'));
+        
+        }
+    }
+}
